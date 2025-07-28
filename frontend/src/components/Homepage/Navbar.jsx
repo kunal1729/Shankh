34862057e4632fd6bf2522622c04e5b2c93493f6
kunal1729 <102245342+kunal1729@users.noreply.github.com>
2025-07-28 10:27:15 +0545

    Update Navbar.jsx

--- frontend/src/components/Homepage/Navbar.jsx
+++ frontend/src/components/Homepage/Navbar.jsx
@@ -12,8 +12,6 @@ const Navbar = () => {
         <Link style={{fontFamily : "Inter"}} to='Services' smooth={true} duration={800}>Services</Link>
         <Link style={{fontFamily : "Inter"}} to='Testimonials' smooth={true} duration={800}>Testimonials</Link>
         <Link style={{fontFamily : "Inter"}} to='AboutUs' smooth={true} duration={800}>About Us</Link>
-        {/<Link style={{fontFamily : "Inter"}} to='Blog' smooth={true} duration={800}>Blog</Link>/}
-        {/<Link style={{fontFamily : "Inter"}} to='Clients' smooth={true} duration={800}>Clients</Link>/}
         <Link style={{fontFamily : "Inter"}} to='ContactUs' smooth={true} duration={800}>Contact Us</Link>
         <NavLink to={'/orgLogin'} style={{fontFamily : "Poppins"}} className='pt-[10px] hover:shadow-lg cursor-pointer pb-[10px] pr-[53px] pl-[53px] rounded-md text-sm w-[199px] font-semibold text-white bg-[#34856C] cursor-pointer'>Get Started</NavLink>
       </div>
